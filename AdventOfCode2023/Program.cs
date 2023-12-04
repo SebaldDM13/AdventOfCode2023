569e66d0086660797a5da0e980ed366972aa9b5c
David-Sebald <SebaldDM13@gmail.com>
2023-12-03 21:35:38 -0600

    Day 03: Part 1

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,23 +1,30 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2023;
 
 public static partial class Program
 {
-    [GeneratedRegex(@".*?(\d|zero|one|two|three|four|five|six|seven|eight|nine)")]
-    private static partial Regex FirstDigit();
-
-    [GeneratedRegex(@".*(\d|zero|one|two|three|four|five|six|seven|eight|nine)")]
-    private static partial Regex LastDigit();
-
     static void Main()
     {
         Console.WriteLine("Advent of Code 2023");
         Console.WriteLine();
+        Day01();
+        Day02();
+        Day03();
+    }
 
+    [GeneratedRegex(@".*?(\d|zero|one|two|three|four|five|six|seven|eight|nine)")]
+    private static partial Regex FirstDigit();
+
+    [GeneratedRegex(@"(\d|zero|one|two|three|four|five|six|seven|eight|nine).*?", RegexOptions.RightToLeft)]
+    private static partial Regex LastDigit();
+
+    static void Day01()
+    {
         Console.WriteLine("Day 01: Part 1");
         string[] lines = File.ReadAllLines(Path.Combine("Input", "Day01.txt"));
-        Console.WriteLine(lines.Select(s => (s.First(c => Char.IsDigit(c)) - '0') * 10 + s.Last(c => Char.IsDigit(c)) - '0').Sum());
+        Console.WriteLine(lines.Select(s => (s.First(Char.IsDigit) - '0') * 10 + s.Last(Char.IsDigit) - '0').Sum());
         Console.WriteLine();
 
         Console.WriteLine("Day 01: Part 2");
@@ -35,17 +42,20 @@ public static partial class Program
             "0" or "zero" or _ => 0
         };
 
-        int sum = 0;
+        int sumOfCalibrationValues = 0;
         foreach (string line in lines)
         {
-            sum += DigitToValue(FirstDigit().Match(line).Groups[1].Value) * 10;
-            sum += DigitToValue(LastDigit().Match(line).Groups[1].Value);
+            sumOfCalibrationValues += DigitToValue(FirstDigit().Match(line).Groups[1].Value) * 10;
+            sumOfCalibrationValues += DigitToValue(LastDigit().Match(line).Groups[1].Value);
         }
-        Console.WriteLine(sum);
+        Console.WriteLine(sumOfCalibrationValues);
         Console.WriteLine();
+    }
 
+    static void Day02()
+    {
         Console.WriteLine("Day 02: Part 1");
-        lines = File.ReadAllLines(Path.Combine("Input", "Day02.txt"));
+        string[] lines = File.ReadAllLines(Path.Combine("Input", "Day02.txt"));
         int gameNumberSum = 0;
         int setPowerSum = 0;
         Dictionary<string, int> cubes = [];
@@ -78,6 +88,67 @@ public static partial class Program
 
         Console.WriteLine("Day 02: Part 2");
         Console.WriteLine(setPowerSum);
+        Console.WriteLine();
+    }
+
+    static void Day03()
+    {
+        Console.WriteLine("Day 03: Part 1");
+        string[] lines = File.ReadAllLines(Path.Combine("Input", "Day03.txt"));
+        StringBuilder numberText = new();
+        int sumOfPartNumbers = 0;
+        bool isAdjacentToSymbol = false;
+
+        char At(int row, int col)
+        {
+            if (row < 0 || row >= lines.Length || col < 0 || col >= lines[0].Length)
+                return '.';
+
+            return lines[row][col];
+        }
+
+        static bool IsSymbol(char c) => !(c == '.' || char.IsDigit(c));
+        void EndOfNumber()
+        {
+            if (numberText.Length > 0 && isAdjacentToSymbol)
+            {
+                sumOfPartNumbers += int.Parse(numberText.ToString());
+            }
+
+            numberText.Clear();
+        }        
+
+        for (int row = 0; row < lines.Length; row++)
+        {            
+            for (int col = 0; col < lines[row].Length; col++)
+            {
+                char top = At(row - 1, col);
+                char middle = At(row, col);
+                char bottom = At(row + 1, col);
+
+                if (char.IsDigit(middle))
+                {
+                    isAdjacentToSymbol = isAdjacentToSymbol || IsSymbol(top) || IsSymbol(bottom);
+                    numberText.Append(middle);
+                }
+                else if (middle == '.')
+                {
+                    isAdjacentToSymbol = isAdjacentToSymbol || IsSymbol(top) || IsSymbol(bottom);
+                    EndOfNumber();
+                    isAdjacentToSymbol = IsSymbol(top) || IsSymbol(bottom);
+                }
+                else
+                {
+                    isAdjacentToSymbol = true;
+                    EndOfNumber();
+                }
+            }
+
+            EndOfNumber();
+            isAdjacentToSymbol = false;
+        }
+        
+        Console.WriteLine(sumOfPartNumbers);
         Console.WriteLine();
     }
 }
